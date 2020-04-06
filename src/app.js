810b9eff8704b85c36bc535e8e7b673e8f7e5050
fbFideles <felipebreno106@gmail.com>
2020-04-06 14:14:13 -0300

    Reupload. Not deploy build

--- src/app.js
+++ src/app.js
@@ -4,14 +4,15 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+require('dotenv/config')
+
 app.use(cors())
 app.use(express.json())
 
 mongoose.connect(`${process.env.MONGO_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-})
-
+}) 
 require('./model/Task')
 
 app.use(require('./routes'))
--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Task = mongoose.model('Task')
 
 module.exports = {
-    async index(req, res) {
+    async index(req,res) {
         const tasks = await Task.find()
 
         return res.json(tasks)
