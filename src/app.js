eb30bab79cd31883db44dd5d08e3b1f53c5051b1
Simone <simo.sprea1907@gmail.com>
2021-10-26 17:15:18 +0200

    security improvements

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const helmet = require('helmet')
+const compression = require('compression')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use(compression);   //decrease the size of the response body
+
+app.use(helmet.hidePoweredBy());    //remove the X-Powered-By header
+//app.use(helmet.hsts());   //sets Strict-Transport-Security header to force use https
 
 mongoose.connect(`${process.env.MONGO_URL}`, {
     useNewUrlParser: true,
